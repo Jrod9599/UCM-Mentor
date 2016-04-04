173b15e97c8aed8f9d6909eb42e20449b6b987b8
abchi1994 <abchi1994@gmail.com>
2016-04-03 17:38:11 -0700

    Commit UI for revision

--- app/src/main/java/Student.java
+++ app/src/main/java/Student.java
@@ -1,6 +1,7 @@
 /**
  * Created by Alex on 2/28/2016.
  */
+//Base skeleton Java class For student, parent of Mentor and Mentee Classes
 public class Student {
     private String name;
     private String email;
