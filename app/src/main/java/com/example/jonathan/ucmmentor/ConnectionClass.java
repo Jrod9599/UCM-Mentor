ba8b043a4f2c4e19dfe7fbda0dd70a8c6fde0bef
abchi1994 <abchi1994@gmail.com>
2016-04-17 18:48:10 -0700

    Trial

---
+++ app/src/main/java/com/example/jonathan/ucmmentor/ConnectionClass.java
@@ -0,0 +1,49 @@
+package com.example.jonathan.ucmmentor;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+/**
+ * Created by Jonathan_Rodriguez on 3/23/2016.
+ */
+public class ConnectionClass {
+    String ip = "192.168.1.2";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "ucm_mentor";
+    String un = "Jrod9599";
+    String password = "November_R14n";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+
+}
+
+/*
+ *
+ */
+
